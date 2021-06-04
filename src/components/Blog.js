7c44335db5efa678b2dda253452f85a8967b93cd
OnlyGod Ovbije <onlygodovbije@gmail.com>
2021-06-04 09:52:42 +0100

    pull blogs from backend 👌

--- src/components/Blog.js
+++ src/components/Blog.js
@@ -1,85 +1,129 @@
 import React, {useState, useEffect} from 'react';
 import axios from 'axios'
+import {Link} from "react-router-dom"
+import {REACT_APP_API_URL} from "./plugins"
 
-const [blogs, setBlogs] = useState([])
+
+
+function Blog() {
+
+
+    const [blogs, setBlogs] = useState([])
 const [featuredBlog, setFeaturedBlog] = useState([])
 
 
 useEffect(() =>{
-    const FetchData = async () =>{
+    const fetchData = async () =>{
        try{
-           const res = await axios
-       }
-       catch(e){
+           const res = await axios.get(`${REACT_APP_API_URL}/api/blog/featured/`);
+           setFeaturedBlog(res.data[0]);
+           console.log(res.data)
 
        }
+       catch(error){
+        console.log(error)
 
-    }
 
+       }
+
+    }   
+    fetchData()
 }, [])
 
 
-function Blog() {
+useEffect(() =>{
+    const fetchBlog = async () =>{
+       try{
+           const res = await axios.get(`${REACT_APP_API_URL}/api/blog/`);
+           setBlogs(res.data);
+
+       }
+       catch(error){
+           console.log(error);
+
+       }
+
+    }   
+    fetchBlog()
+}, []);
+
+const capitalizeFirstLetter = (word) =>{
+    if(word){
+        return word.charAt(0).toUpperCase() + word.slice(1);
+   
+    }
+    return '';
+
+}
+
+
+const getBlogs = () => {
+    let list = [];
+    let result = [];
+
+    blogs.map(blogPost =>{
+        return list.push(
+            <div className="row g-0 border rounded overflow-hidden flex-md-row mb-4 shadow-sm h-md-250 position-relative">
+                <div className="col p-4 d-flex flex-column position-static">
+                    <strong className="d-inline-block mb-2 text-primary">{blogPost.category}</strong>
+                    <h3 className="mb-0">Featured post</h3>
+                    <div className="mb-1 text-muted">Nov 12</div>
+                    <p className="card-text mb-auto">This is a wider card with supporting text below as a natural lead-in to additional content.</p>
+                    <Link href="/" className="stretched-link">Continue reading</Link>
+                </div>
+                <div className="col-auto d-none d-lg-block">
+                <svg className="bd-placeholder-img" width="200" height="250" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="/55595c"></rect><text x="50%" y="50%" fill="/eceeef" dy=".3em">Thumbnail</text></svg>
+                </div>
+        </div>
+        )
+    });
+
+    for(let i = 0; i<list.length; i+=2 ){
+        result.push(
+            <div key={i} className="row mb-2">
+               <div className="col-md-6">
+               {list[i]}
+               </div>
+                <div className="col-md-6">
+                    {list[i+1] ? list[i+1] : null}
+        
+                 </div>
+        </div>
+        )
+
+    }
+
+    return result;
+        
+
+    
+};
     return (
         
 <React.Fragment >
-<div class="nav-scroller py-1 mb-2">
-    <nav class="nav d-flex justify-content-between">
-      <a class="p-2 link-secondary" href="#">World</a>
-      <a class="p-2 link-secondary" href="#">U.S.</a>
-      <a class="p-2 link-secondary" href="#">Technology</a>
-      <a class="p-2 link-secondary" href="#">Design</a>
-      <a class="p-2 link-secondary" href="#">Culture</a>
-      <a class="p-2 link-secondary" href="#">Business</a>
-      <a class="p-2 link-secondary" href="#">Politics</a>
-      <a class="p-2 link-secondary" href="#">Opinion</a>
-      <a class="p-2 link-secondary" href="#">Science</a>
-      <a class="p-2 link-secondary" href="#">Health</a>
-      <a class="p-2 link-secondary" href="#">Style</a>
-      <a class="p-2 link-secondary" href="#">Travel</a>
+<div className="nav-scroller py-1 mb-2">
+    <nav className="nav d-flex justify-content-between">
+      <Link className="p-2 link-secondary" to="/category/love">Love</Link>
+      <Link className="p-2 link-secondary" to="/category/God">U.S.</Link>
+      <Link className="p-2 link-secondary" to="/category/code">Technology</Link>
+      <Link className="p-2 link-secondary" to="/category/love">Design</Link>
+      <Link className="p-2 link-secondary" to="/category/love">Culture</Link>
+      <Link className="p-2 link-secondary" to="/category/love">Business</Link>
+      <Link className="p-2 link-secondary" to="/category/love">Politics</Link>
+      <Link className="p-2 link-secondary" to="/category/love">Opinion</Link>
+      
     </nav>
   </div>
-<main class="container">
-    <div class="p-4 p-md-5 mb-4 text-white rounded bg-dark">
-        <div class="col-md-6 px-0">
-        <h1 class="display-4 fst-italic">Title of a longer featured blog post</h1>
-        <p class="lead my-3">Multiple lines of text that form the lede, informing new readers quickly and efficiently about what’s most interesting in this post’s contents.</p>
-        <p class="lead mb-0"><a href="#" class="text-white fw-bold">Continue reading...</a></p>
+<main className="container">
+    <div className="p-4 p-md-5 mb-4 text-white rounded bg-dark">
+        <div className="col-md-6 px-0">
+        <h1 className="display-4 fst-italic">{featuredBlog.title}</h1>
+        <p className="lead my-3">{featuredBlog.excerpt}</p>
+        <p className="lead mb-0"><Link to={`/blog/${featuredBlog.slug}`} className="text-white fw-bold">Continue reading...</Link></p>
         </div>
     </div>
 
-    <div class="row mb-2">
-        <div class="col-md-6">
-        <div class="row g-0 border rounded overflow-hidden flex-md-row mb-4 shadow-sm h-md-250 position-relative">
-            <div class="col p-4 d-flex flex-column position-static">
-            <strong class="d-inline-block mb-2 text-primary">World</strong>
-            <h3 class="mb-0">Featured post</h3>
-            <div class="mb-1 text-muted">Nov 12</div>
-            <p class="card-text mb-auto">This is a wider card with supporting text below as a natural lead-in to additional content.</p>
-            <a href="#" class="stretched-link">Continue reading</a>
-            </div>
-            <div class="col-auto d-none d-lg-block">
-            <svg class="bd-placeholder-img" width="200" height="250" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"></rect><text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text></svg>
-
-            </div>
-        </div>
-        </div>
-        <div class="col-md-6">
-        <div class="row g-0 border rounded overflow-hidden flex-md-row mb-4 shadow-sm h-md-250 position-relative">
-            <div class="col p-4 d-flex flex-column position-static">
-            <strong class="d-inline-block mb-2 text-success">Design</strong>
-            <h3 class="mb-0">Post title</h3>
-            <div class="mb-1 text-muted">Nov 11</div>
-            <p class="mb-auto">This is a wider card with supporting text below as a natural lead-in to additional content.</p>
-            <a href="#" class="stretched-link">Continue reading</a>
-            </div>
-            <div class="col-auto d-none d-lg-block">
-            <svg class="bd-placeholder-img" width="200" height="250" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"></rect><text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text></svg>
-
-            </div>
-        </div>
-        </div>
-    </div>
+    {getBlogs()}
 
 </main>
 </React.Fragment>
---
+++ src/components/plugins.js
@@ -0,0 +1,1 @@
+export const REACT_APP_API_URL = 'http://localhost:8000'    
